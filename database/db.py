f3911ac84f981816262138f7ff2da9f56019e7ef
SA Romanchuk <gefest@me.com>
2021-04-02 20:10:51 +0300

    les3 hw

--- database/db.py
+++ database/db.py
@@ -4,43 +4,84 @@ from sqlalchemy.orm import sessionmaker
 from . import models
 
 
-table_mapper = {
-    "post_data": models.Post,
-    "author_data": models.Author,
-}
-
-
 class Database:
     def __init__(self, db_url):
         engine = create_engine(db_url)
         models.Base.metadata.create_all(bind=engine)
         self.maker = sessionmaker(bind=engine)
 
-    def get_or_create(self, session, model, filter, **data):
-        db_instance = session.query(model).filter_by(**filter).first()
-        if not db_instance:
-            db_instance = model(**data)
-        return db_instance
+    def _get_or_create(self, session, model, uniq_field, uniq_value, **data):
+        db_data = session.query(model).filter(uniq_field == uniq_value).first()
+        if not db_data:
+            db_data = model(**data)
+            session.add(db_data)
+            try:
+                session.commit()
+            except Exception as exc:
+                print(exc)
+                session.rollback()
+        return db_data
+
+    def _get_or_create_comments(self, session, data: list) -> list:
+        result = []
+        if data:
+            for comment in data:
+                comment_author = self._get_or_create(
+                    session,
+                    models.Author,
+                    models.Author.url,
+                    comment["comment"]["user"]["url"],
+                    name=comment["comment"]["user"]["full_name"],
+                    url=comment["comment"]["user"]["url"],
+                )
+                db_comment = self._get_or_create(
+                    session,
+                    models.Comment,
+                    models.Comment.id,
+                    comment["comment"]["id"],
+                    **comment["comment"],
+                    author=comment_author,
+                )
+
+                result.append(db_comment)
+                result.extend(
+                    self._get_or_create_comments(session, comment["comment"]["children"])
+                )
+
+        return result
 
     def create_post(self, data):
         session = self.maker()
-        post = None
-        for key, model in table_mapper.items():
-            instance = self.get_or_create(session, model, {"url": data[key]["url"]}, **data[key])
-            if isinstance(instance, models.Post):
-                post = instance
-            elif isinstance(instance, models.Author):
-                post.author = instance
-        post.tags.extend(
-            [
-                self.get_or_create(session, models.Tag, {"url": tag_data["url"]}, **tag_data)
-                for tag_data in data["tags_data"]
-            ]
+        comments = self._get_or_create_comments(session, data["comments_data"])
+        author = self._get_or_create(
+            session,
+            models.Author,
+            models.Author.url,
+            data["author_data"]["url"],
+            **data["author_data"],
         )
+        tags = map(
+            lambda tag_data: self._get_or_create(
+                session, models.Tag, models.Tag.url, tag_data["url"], **tag_data
+            ),
+            data["tags_data"],
+        )
+        post = self._get_or_create(
+            session,
+            models.Post,
+            models.Post.id,
+            data["post_data"]["id"],
+            **data["post_data"],
+            author=author,
+        )
+        post.tags.extend(tags)
+        post.comments.extend(comments)
+        session.add(post)
+
         try:
-            session.add(post)
             session.commit()
-        except Exception:
+        except Exception as exc:
+            print(exc)
             session.rollback()
         finally:
             session.close()
--- gb_blog_parse.py
+++ gb_blog_parse.py
@@ -1,31 +1,32 @@
-import typing
 import requests
 import bs4
 from urllib.parse import urljoin
-from database import db
+from database.db import Database
 
 
 class GbBlogParse:
-    def __init__(self, start_url, database: db.Database):
+    def __init__(self, start_url, database: Database):
         self.db = database
         self.start_url = start_url
         self.done_urls = set()
-        self.tasks = []
+        self.tasks = [
+            self.get_task(self.start_url, self.parse_feed),
+        ]
+        self.done_urls.add(self.start_url)
 
-    def get_task(self, url: str, callback: typing.Callable) -> typing.Callable:
+    def get_task(self, url, callback):
         def task():
             soup = self._get_soup(url)
             return callback(url, soup)
 
         return task
 
-    def _get_response(self, url, *args, **kwargs) -> requests.Response:
-        # TODO: Обработать статус коды и ошибки
-        response = requests.get(url, *args, **kwargs)
+    def _get_response(self, url):
+        response = requests.get(url)
         return response
 
-    def _get_soup(self, url, *args, **kwargs) -> bs4.BeautifulSoup:
-        soup = bs4.BeautifulSoup(self._get_response(url, *args, **kwargs).text, "lxml")
+    def _get_soup(self, url):
+        soup = bs4.BeautifulSoup(self._get_response(url).text, "lxml")
         return soup
 
     def parse_post(self, url, soup):
@@ -34,57 +35,59 @@ class GbBlogParse:
             "post_data": {
                 "title": soup.find("h1", attrs={"class": "blogpost-title"}).text,
                 "url": url,
+                "id": soup.find("comments").attrs.get("commentable-id"),
             },
             "author_data": {
                 "url": urljoin(url, author_tag.parent.attrs.get("href")),
                 "name": author_tag.text,
             },
             "tags_data": [
-                {"url": urljoin(url, tag_a.attrs.get("href")), "name": tag_a.text}
-                for tag_a in soup.find_all("a", attrs={"class": "small"})
+                {"name": tag.text, "url": urljoin(url, tag.attrs.get("href"))}
+                for tag in soup.find_all("a", attrs={"class": "small"})
             ],
+            "comments_data": self._get_comments(soup.find("comments").attrs.get("commentable-id")),
         }
+        return data
+
+    def _get_comments(self, post_id):
+        api_path = f"/api/v2/comments?commentable_type=Post&commentable_id={post_id}&order=desc"
+        response = self._get_response(urljoin(self.start_url, api_path))
+        data = response.json()
         return data
 
     def parse_feed(self, url, soup):
         ul = soup.find("ul", attrs={"class": "gb__pagination"})
         pag_urls = set(
-            urljoin(url, url_a.attrs.get("href"))
-            for url_a in ul.find_all("a")
-            if url_a.attrs.get("href")
+            urljoin(url, href.attrs.get("href"))
+            for href in ul.find_all("a")
+            if href.attrs.get("href")
         )
         for pag_url in pag_urls:
             if pag_url not in self.done_urls:
-                task = self.get_task(pag_url, self.parse_feed)
-                self.done_urls.add(pag_url)
-                self.tasks.append(task)
+                self.tasks.append(self.get_task(pag_url, self.parse_feed))
 
-        post_urls = set(
-            urljoin(url, url_a.attrs.get("href"))
-            for url_a in soup.find_all("a", attrs={"class": "post-item__title"})
-            if url_a.attrs.get("href")
+        post_items = soup.find("div", attrs={"class": "post-items-wrapper"})
+        posts_urls = set(
+            urljoin(url, href.attrs.get("href"))
+            for href in post_items.find_all("a", attrs={"class": "post-item__title"})
+            if href.attrs.get("href")
         )
-        for post_url in post_urls:
+
+        for post_url in posts_urls:
             if post_url not in self.done_urls:
-                task = self.get_task(post_url, self.parse_post)
-                self.done_urls.add(post_url)
-                self.tasks.append(task)
+                self.tasks.append(self.get_task(post_url, self.parse_post))
 
     def run(self):
-        task = self.get_task(self.start_url, self.parse_feed)
-        self.tasks.append(task)
-        self.done_urls.add(self.start_url)
-
         for task in self.tasks:
             task_result = task()
             if task_result:
-                self.save(task_result)
+                self.db.create_post(task_result)
 
     def save(self, data):
         self.db.create_post(data)
 
 
 if __name__ == "__main__":
-    database = db.Database("sqlite:///db_blog.db")
+    database = Database("sqlite:///gb_blog.db")
     parser = GbBlogParse("https://geekbrains.ru/posts", database)
     parser.run()
