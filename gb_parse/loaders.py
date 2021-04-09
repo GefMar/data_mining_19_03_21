68f3be34278e5c5cac93c089d839658f8175fc67
SA Romanchuk <gefest@me.com>
2021-04-09 21:44:31 +0300

    les6

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -1,4 +1,6 @@
 import re
+from urllib.parse import urljoin
+
 from scrapy.loader import ItemLoader
 from scrapy import Selector
 from itemloaders.processors import TakeFirst, MapCompose
@@ -26,7 +28,10 @@ def get_characteristics(item: str) -> dict:
 def get_author_id(text):
     re_pattern = re.compile(r"youlaId%22%2C%22([a-zA-Z|\d]+)%22%2C%22avatar")
     result = re.findall(re_pattern, text)
-    user_link = f"https://youla.ru/user/{result[0]}"
+    try:
+        user_link = f"https://youla.ru/user/{result[0]}"
+    except IndexError:
+        user_link = ""
     return user_link
 
 
@@ -39,4 +44,23 @@ class AutoyoulaLoader(ItemLoader):
     characteristics_in = MapCompose(get_characteristics)
     description_out = TakeFirst()
     author_in = MapCompose(get_author_id)
+    author_out = TakeFirst()
+
+
+def flat_text(items):
+    return "\n".join(items)
+
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
+
+class HHLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    salary_out = flat_text
+    # description_in = flat_text,
+    # description_out = flat_text,
+    author_in = MapCompose(hh_user_url)
     author_out = TakeFirst()
